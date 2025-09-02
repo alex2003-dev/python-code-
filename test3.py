057dc3afa33d57cca409727132b4ae403b15e32c
alex2003-dev <justsanchez241@gmail.com>
2025-09-02 20:48:10 +0300

    test3.py

--- test3.py
+++ test3.py
@@ -1,22 +1,18 @@
-# Simple text analyzer
 sentence = input("Type a sentence: ")
 sentence = sentence.strip()
 
 if sentence == "":
     print("Ты ничего не написал.")
 else:
-    # Calculate sentence length
+    
     length = len(sentence)
 
-    # Split into words
     words = sentence.split()
     word_count = len(words)
 
-    # Get first and last character
     first_letter = sentence[0]
     last_letter = sentence[-1]
-
-    # Print banner
+ 
     print("+" + "-" * 40 + "+")
     print("| Sentence: " + sentence)
     print("| Length:", length)
